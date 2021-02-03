c0b11cd869e658d017e39edfa0579290e03086bd
HaopengSun <sunhaopeng@gmail.com>
2021-02-03 22:44:14 +0000

    change it for testing reservation data retrive

--- 1_queries/all_my_reservation.sql
+++ 1_queries/all_my_reservation.sql
@@ -1,12 +1,32 @@
+-- SELECT properties.id as id,
+-- properties.title as title,
+-- reservations.start_date as start_date,
+-- properties.cost_per_night as cost_per_night,
+-- avg(property_reviews.rating) as rating
+-- FROM reservations
+-- JOIN property_reviews ON property_reviews.reservation_id=reservations.id
+-- JOIN properties ON properties.id=reservations.property_id
+-- WHERE reservations.end_date<now()::date AND reservations.guest_id=1001
+-- GROUP BY properties.id, reservations.id
+-- ORDER BY start_date
+-- LIMIT 10;
+
 SELECT properties.id as id,
 properties.title as title,
 reservations.start_date as start_date,
+reservations.end_date as end_date,
+properties.number_of_bathrooms as number_of_bedrooms,
+properties.number_of_bedrooms as number_of_bathrooms,
+properties.parking_spaces as parking_spaces,
 properties.cost_per_night as cost_per_night,
-avg(property_reviews.rating) as rating
+properties.thumbnail_photo_url as thumbnail_photo_url,
+properties.cover_photo_url as cover_photo_url,
+avg(rating) as average_rating
 FROM reservations
-JOIN property_reviews ON property_reviews.reservation_id=reservations.id
-JOIN properties ON properties.id=reservations.property_id
-WHERE reservations.end_date<now()::date AND reservations.guest_id=1
+JOIN properties ON reservations.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id 
+WHERE reservations.guest_id = 1001
+AND reservations.end_date < now()::date
 GROUP BY properties.id, reservations.id
-ORDER BY start_date
+ORDER BY reservations.start_date
 LIMIT 10;
