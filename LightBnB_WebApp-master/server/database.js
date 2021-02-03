08d3788c115ffb8e6158fb364b48fc8981d323a9
HaopengSun <sunhaopeng@gmail.com>
2021-02-03 22:43:35 +0000

    add reservation data from db

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -18,7 +18,10 @@ const pool = new Pool({
  */
 const getUserWithEmail = function(email) {
   return pool.query(`SELECT * FROM users WHERE users.email=$1`, [email])
-  .then(res => res.rows[0]);
+  .then(res => {
+    console.log(res.rows[0]);
+    return res.rows[0];
+  });
 }
 exports.getUserWithEmail = getUserWithEmail;
 
@@ -57,7 +60,30 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT properties.id as id,
+  properties.title as title,
+  reservations.start_date as start_date,
+  reservations.end_date as end_date,
+  properties.number_of_bathrooms as number_of_bedrooms,
+  properties.number_of_bedrooms as number_of_bathrooms,
+  properties.parking_spaces as parking_spaces,
+  properties.cost_per_night as cost_per_night,
+  properties.thumbnail_photo_url as thumbnail_photo_url,
+  properties.cover_photo_url as cover_photo_url,
+  avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `, [guest_id, limit]).then(res => {
+    // console.log(guest_id, res.rows);
+    return res.rows;
+  });
 }
 exports.getAllReservations = getAllReservations;
 
